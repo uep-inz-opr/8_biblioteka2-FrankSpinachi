fc7049d514c0ec435cafe46605f3840e5461629d
FrankSpinachi <76732249+FrankSpinachi@users.noreply.github.com>
2022-05-24 22:59:56 +0200

    v1

---
+++ main.py
@@ -0,0 +1,60 @@
+biblioteka = {}
+uzytkownicy = {}
+class Czytelnik:
+    def __init__(self, imie):
+        self.imie = imie
+        self.ksiazki_czytelnika = []
+
+class Ksiazka:
+    def __init__(self, tytul, autor, rok_wydania):
+        self.tytul = tytul
+        self.autor = autor
+        self.rok_wydania = rok_wydania
+        self.ilosc = 1
+
+def dodaj_ksiazke(tytul, autor, rok_wydania):
+    if tytul in biblioteka: 
+        biblioteka[tytul].ilosc += 1
+    else:
+        biblioteka[tytul] = Ksiazka(tytul, autor, rok_wydania)
+    return True
+
+
+def wypozycz_ksiazke(imie, tytul):
+    if imie in uzytkownicy:
+        if len(uzytkownicy[imie].ksiazki_czytelnika) < 3:
+            biblioteka[tytul].ilosc -= 1
+            uzytkownicy[imie].ksiazki_czytelnika.append(tytul)
+            return True
+        else:
+            return False
+    else:
+        uzytkownicy[imie] = Czytelnik(imie)
+        biblioteka[tytul].ilosc -= 1
+        uzytkownicy[imie].ksiazki_czytelnika.append(tytul)
+        return True 
+
+
+def operacje(komenda, tytul, autor, rok_wydania = None):
+    if komenda == 'dodaj':
+        return dodaj_ksiazke(tytul, autor, rok_wydania)
+        
+    elif komenda == 'wypozycz':
+        return wypozycz_ksiazke(imie=tytul, tytul=autor)
+    
+
+if __name__ == "__main__":
+    n = input()
+
+    for i in range(int(n)):
+        x = eval(input())
+        if x[0].strip() == 'dodaj':
+            print(operacje(x[0].strip(), x[1].strip(), x[2].strip(), x[3] ))
+        else:
+            print(operacje(x[0].strip(), x[1].strip(), x[2].strip()))
+
+
+
+
+
+    
